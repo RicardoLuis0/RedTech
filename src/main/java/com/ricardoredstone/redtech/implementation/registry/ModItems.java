e15eac1b006ea76f7fa70747c86867fb1f6c909a
RicardoLuis0 <ricolvs123@gmail.com>
2019-11-11 14:51:11 -0300

    ettroite alloy mix, for making ettroite alloy from iron+ettroite, needs blast furnace for smelting

--- src/main/java/com/ricardoredstone/redtech/implementation/registry/ModItems.java
+++ src/main/java/com/ricardoredstone/redtech/implementation/registry/ModItems.java
@@ -6,6 +6,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 
 public class ModItems {
+    public final ModSimpleItem ETTROITE_ALLOY_MIX = (ModSimpleItem) RedTechMod.MOD_REGISTRY.addObject(new ModSimpleItem("ettroite_alloy_mix", new Item.Properties().group(ItemGroup.MATERIALS)));
     public final ModSimpleItem ETTROITE_DUST = (ModSimpleItem) RedTechMod.MOD_REGISTRY.addObject(new ModSimpleItem("ettroite_dust", new Item.Properties().group(ItemGroup.MATERIALS)));
     public final ModSimpleItem IRON_DUST = (ModSimpleItem) RedTechMod.MOD_REGISTRY.addObject(new ModSimpleItem("iron_dust", new Item.Properties().group(ItemGroup.MATERIALS)));
     public final ModSimpleItem GOLD_DUST = (ModSimpleItem) RedTechMod.MOD_REGISTRY.addObject(new ModSimpleItem("gold_dust", new Item.Properties().group(ItemGroup.MATERIALS)));

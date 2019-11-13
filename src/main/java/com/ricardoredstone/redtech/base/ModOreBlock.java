b67a9ecb0dc231c136f848cb8340abd8cf7fd8dd
RicardoLuis0 <ricolvs123@gmail.com>
2019-11-13 19:14:43 -0300

    fix

--- src/main/java/com/ricardoredstone/redtech/base/ModOreBlock.java
+++ src/main/java/com/ricardoredstone/redtech/base/ModOreBlock.java
@@ -16,17 +16,22 @@ public class ModOreBlock extends ModSimpleBlock {
 
     private final int count;
     private final CountRangeConfig spawn_range;
+    private final OreFeatureConfig.FillerBlockType target;
 
-    public ModOreBlock(String name,int count,CountRangeConfig spawn_range) {
+    public ModOreBlock(String name,int count,CountRangeConfig spawn_range){
+        this(name,count,spawn_range,false);
+    }
+    public ModOreBlock(String name,int count,CountRangeConfig spawn_range,boolean nether) {
         super(name, Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0F,3.0F));
         this.count=count;
         this.spawn_range=spawn_range;
+        this.target=nether?OreFeatureConfig.FillerBlockType.NETHERRACK:OreFeatureConfig.FillerBlockType.NATURAL_STONE;
     }
 
     @Override
     public void setupCommon(final FMLCommonSetupEvent event) {
         for(Biome biome : ForgeRegistries.BIOMES) {
-            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, RedTechMod.BLOCKS.ETTROITE_ORE.getDefaultState(), count), Placement.COUNT_RANGE, spawn_range));
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(target, getDefaultState(), count), Placement.COUNT_RANGE, spawn_range));
         }
     }
 }

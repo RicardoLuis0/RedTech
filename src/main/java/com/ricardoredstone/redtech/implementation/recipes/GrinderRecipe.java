f9a95f7b39451473cd6c5d9e05580cd6585a6690
RicardoLuis0 <ricolvs123@gmail.com>
2019-11-14 00:34:41 -0300

    grinder recipe WIP

--- src/main/java/com/ricardoredstone/redtech/implementation/recipes/GrinderRecipe.java
+++ src/main/java/com/ricardoredstone/redtech/implementation/recipes/GrinderRecipe.java
@@ -6,6 +6,7 @@ import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.*;
 import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.registries.ForgeRegistryEntry;
@@ -13,50 +14,79 @@ import net.minecraftforge.registries.ForgeRegistryEntry;
 import javax.annotation.Nullable;
 
 public class GrinderRecipe implements IRecipe<IInventory> {
-    private static class GrinderRecipeSerializer extends ForgeRegistryEntry<IRecipeSerializer<?>> implements IRecipeSerializer<GrinderRecipe> {
+    private static class Serializer extends ForgeRegistryEntry<IRecipeSerializer<?>> implements IRecipeSerializer<GrinderRecipe> {
 
         @Override
         public GrinderRecipe read(ResourceLocation recipeId, JsonObject json) {
-            return null;
+            throw new RuntimeException("GrinderRecipe::Serializer::read unimplemented");
         }
 
         @Nullable
         @Override
         public GrinderRecipe read(ResourceLocation recipeId, PacketBuffer buffer) {
-            return null;
+            throw new RuntimeException("GrinderRecipe::Serializer::read unimplemented");
         }
 
         @Override
         public void write(PacketBuffer buffer, GrinderRecipe recipe) {
-            return;
+            throw new RuntimeException("GrinderRecipe::Serializer::write unimplemented");
         }
     }
 
-    public static final IRecipeSerializer<GrinderRecipe> SERIALIZER = new GrinderRecipeSerializer();
+    private static class GrinderRecipeType implements IRecipeType<GrinderRecipe> {
+        public String toString(){
+            return "grinder";
+        }
+    }
 
+    public static final IRecipeSerializer<GrinderRecipe> SERIALIZER = new Serializer();
+    public static final IRecipeType<GrinderRecipe> TYPE = new GrinderRecipeType();
+    public final ResourceLocation id;
+    public final String group;
+    private final Ingredient ingredient;
+    private final ItemStack output;
+
+    GrinderRecipe(String name,String group,Ingredient ingredient,ItemStack output){
+        id=RedTechMod.makeResourceLocation(name);
+        this.ingredient=ingredient;
+        this.output=output;
+        this.group=group;
+    }
     @Override
     public boolean matches(IInventory inv, World worldIn) {
-        return false;
+        throw new RuntimeException("GrinderRecipe::matches unimplemented");//TODO
     }
 
     @Override
     public ItemStack getCraftingResult(IInventory inv) {
-        return null;
+        return output.copy();
     }
 
     @Override
     public boolean canFit(int width, int height) {
-        return false;
+        return true;
     }
 
     @Override
     public ItemStack getRecipeOutput() {
-        return null;
+        return output;
+    }
+
+    @Override
+    public NonNullList<Ingredient> getIngredients() {
+        NonNullList<Ingredient> l = NonNullList.create();
+        l.add(ingredient);
+        return l;
+    }
+
+    @Override
+    public String getGroup() {
+        return group;
     }
 
     @Override
     public ResourceLocation getId() {
-        return null;
+        return id;
     }
 
     @Override
@@ -66,6 +96,6 @@ public class GrinderRecipe implements IRecipe<IInventory> {
 
     @Override
     public IRecipeType<?> getType() {
-        return null;
+        return TYPE;
     }
 }

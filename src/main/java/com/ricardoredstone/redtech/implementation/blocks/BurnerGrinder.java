7e028dcc88598470bec3c4b3a8099bc947af2d72
RicardoLuis0 <ricolvs123@gmail.com>
2019-11-14 05:09:16 -0300

    lit particles

--- src/main/java/com/ricardoredstone/redtech/implementation/blocks/BurnerGrinder.java
+++ src/main/java/com/ricardoredstone/redtech/implementation/blocks/BurnerGrinder.java
@@ -1,24 +1,31 @@
 package com.ricardoredstone.redtech.implementation.blocks;
 
-import com.ricardoredstone.redtech.base.ModDirectionalBlockHorizontal;
 import com.ricardoredstone.redtech.base.ModMachine;
 import com.ricardoredstone.redtech.implementation.blocks.tile_entities.BurnerGrinderTileEntity;
+import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.state.StateContainer;
-import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.ToolType;
 
 import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Random;
 
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
 public class BurnerGrinder extends ModMachine {
     public BurnerGrinder() {
         super("burner_grinder", Block.Properties.create(Material.ROCK).hardnessAndResistance(3.5F).sound(SoundType.STONE).lightValue(13).harvestTool(ToolType.PICKAXE).harvestLevel(0));
@@ -33,5 +40,27 @@ public class BurnerGrinder extends ModMachine {
     @Override
     public TileEntity createNewTileEntity(IBlockReader worldIn) {
         return new BurnerGrinderTileEntity();
+    }
+
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {//adapted from BlastFurnaceBlock::animateTick
+        super.animateTick(stateIn,worldIn,pos,rand);
+        if (stateIn.get(LIT)) {
+            double d0 = pos.getX() + 0.5D;
+            double d1 = pos.getY();
+            double d2 = pos.getZ() + 0.5D;
+            if (rand.nextDouble() < 0.1D) {
+                worldIn.playSound(d0, d1, d2, SoundEvents.BLOCK_BLASTFURNACE_FIRE_CRACKLE, SoundCategory.BLOCKS, 1.0F, 1.0F, false);
+            }
+            Direction direction = stateIn.get(FACING);
+            Direction.Axis direction$axis = direction.getAxis();
+            double d4 = rand.nextDouble() * 0.6D - 0.3D;
+            double d5 = direction$axis == Direction.Axis.X ? (double) direction.getXOffset() * 0.6D : d4;
+            double d6 = rand.nextDouble() * 9.0D / 16.0D;
+            double d7 = direction$axis == Direction.Axis.Z ? (double) direction.getZOffset() * 0.6D : d4;
+            worldIn.addParticle(ParticleTypes.SMOKE, d0 + d5, d1 + d6, d2 + d7, 0.0D, 0.0D, 0.0D);
+        }
     }
 }

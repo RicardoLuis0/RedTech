6c8be9110e3fa95b849d569e7cbf8b93d252172b
RicardoLuis0 <ricolvs123@gmail.com>
2019-11-14 02:46:22 -0300

    Ettroite harvest level stone

--- src/main/java/com/ricardoredstone/redtech/implementation/registry/ModBlocks.java
+++ src/main/java/com/ricardoredstone/redtech/implementation/registry/ModBlocks.java
@@ -6,6 +6,6 @@ import com.ricardoredstone.redtech.implementation.blocks.BurnerGrinder;
 import net.minecraft.world.gen.placement.CountRangeConfig;
 
 public final class ModBlocks {
-    public final ModOreBlock ETTROITE_ORE = RedTechMod.REGISTRY.add(new ModOreBlock("ettroite_ore",8,new CountRangeConfig(10,0,0,48)));
+    public final ModOreBlock ETTROITE_ORE = RedTechMod.REGISTRY.add(new ModOreBlock("ettroite_ore",8,new CountRangeConfig(10,0,0,48),1));
     public final BurnerGrinder BURNER_GRINDER = RedTechMod.REGISTRY.add(new BurnerGrinder());
 }

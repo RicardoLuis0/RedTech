1569d91a8de306b91fda005d181c20fe8ee22d46
RicardoLuis0 <ricolvs123@gmail.com>
2019-11-17 21:28:21 -0300

    class for ore+metal

---
+++ src/main/java/com/ricardoredstone/redtech/base/ModOreMetal.java
@@ -0,0 +1,35 @@
+package com.ricardoredstone.redtech.base;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.world.gen.placement.CountRangeConfig;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+public class ModOreMetal implements ModObject {
+
+    public ModOreBlock ore;
+    public ModMetal metal;
+
+    public ModOreMetal(String name, int count, CountRangeConfig spawn_range){
+        ore=new ModOreBlock(name,count,spawn_range);
+        metal=new ModMetal(name);
+    }
+
+    @Override
+    public void setupCommon(final FMLCommonSetupEvent event) {
+        ore.setupCommon(event);
+    }
+
+    @Override
+    public void registerBlocks(final RegistryEvent.Register<Block> event) {
+        ore.registerBlocks(event);
+        metal.registerBlocks(event);
+    }
+
+    @Override
+    public void registerItems(final RegistryEvent.Register<Item> event) {
+        ore.registerItems(event);
+        metal.registerItems(event);
+    }
+}
